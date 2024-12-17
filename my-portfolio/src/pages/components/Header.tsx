977cb63de2a90a07e9ee7b7cf11e88b7e44819b7
K_Nund <ON144@live.mdx.ac.uk>
2024-12-17 18:14:53 +0400

    -Modifies navigation bar to appear as a hamburger menu in mobile

--- my-portfolio/src/pages/components/Header.tsx
+++ my-portfolio/src/pages/components/Header.tsx
@@ -1,18 +1,46 @@
+"use client";
+
+import {useState} from 'react';
+
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    //toggle function
+    const toggleMobileMenu = () => {
+        setIsMenuOpen((prevState) => !prevState);
+    }
+
     return(
-        <header className="border-x-slate-200 border-2 mb-12">
+        <header className="border-gray-200 border-b-2 mb-12">
              <nav className="flex w-screen items-center justify-between p-6">
                 <a href="/">
                     <img src="logo.png" alt="logo" className="h-10 w-auto"/>
                 </a>
 
-                <div className="flex w-screen items-center justify-evenly">
-                    <a href="" className="text-lg font-semibold leading-6 text-gray-900 hover:underline underline-offset-2">About</a>
-                    <a href="" className="text-lg font-semibold leading-6 text-gray-900 hover:underline underline-offset-2">Projects</a>
-                    <a href="" className="text-lg font-semibold leading-6 text-gray-900 hover:underline underline-offset-2">Skills</a>
+                {/* Desktop navigation bar */}
+                <div className="hidden md:flex w-screen items-center justify-evenly">
+                    <a href="#about" className="text-lg font-semibold leading-6 text-gray-900 hover:underline underline-offset-2">About</a>
+                    <a href="#projects" className="text-lg font-semibold leading-6 text-gray-900 hover:underline underline-offset-2">Projects</a>
+                    <a href="#skills" className="text-lg font-semibold leading-6 text-gray-900 hover:underline underline-offset-2">Skills</a>
                 </div>
-            
+
+                {/* Mobile hamburger menu */}
+                <div className="md:hidden">
+                    <button id="menu-btn" className="focus:outline-none" onClick={toggleMobileMenu}>
+                        <i className="fa-solid fa-bars fa-2xl"></i>
+                    </button>
+                </div>
+
             </nav>
+
+            {/* Mobile Dropdown Menu */}
+            {isMenuOpen && (
+                <ul id="mobile-menu" className="flex-col items-center text-center border-gray-200 border-t-2 md:hidden">
+                    <li><a href="#about" className="block py-2 font-semibold" onClick={toggleMobileMenu}>About</a></li>
+                    <li><a href="#projects" className="block py-2 font-semibold" onClick={toggleMobileMenu}>Projects</a></li>
+                    <li><a href="#skills" className="block py-2 font-semibold" onClick={toggleMobileMenu}>Skills</a></li>
+                </ul>
+            )}
         </header>
        
     );
