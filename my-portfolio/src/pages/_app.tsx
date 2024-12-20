7ff43fbc5cbb73a765d8086c6528a8f848b15064
K_Nund <ON144@live.mdx.ac.uk>
2024-12-20 11:21:07 +0400

    -Adds font awesome icons script link -Adds the necessary modules on index page

--- my-portfolio/src/pages/_app.tsx
+++ my-portfolio/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="logo.png" />
         <title>Krishi's Portfolio</title>
       </Head>
+      <Script src="https://kit.fontawesome.com/df395f211e.js" crossOrigin="anonymous"></Script>
       <Component {...pageProps} />
     </>
   );
--- my-portfolio/src/pages/index.tsx
+++ my-portfolio/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 const Home = () => {
   return(
     <div>
       <Header></Header>
       <Welcome></Welcome>
+      <Projects></Projects>
+      <Skills></Skills>
     </div>
   )
 }
