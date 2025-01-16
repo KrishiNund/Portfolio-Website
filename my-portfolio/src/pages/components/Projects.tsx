9662c780d62e1b41f4222cb8eb0dedf215594ec9
K_Nund <ON144@live.mdx.ac.uk>
2025-01-16 19:53:33 +0400

    -Adds new project and removes arrows from slide show

--- my-portfolio/src/pages/components/Projects.tsx
+++ my-portfolio/src/pages/components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
     speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows:true,
+    arrows:false,
     autoplay:true,
     autoplaySpeed: 8000,
     pauseOnHover:true,
@@ -25,6 +25,7 @@ const Projects = () => {
       description: "It's a tic-tac-toe playing robot that consists of a Yahboom DOFBOT robotic arm, a camera module attached to it and using OpenCV libraries to perform template matching to identify the grid's contents.",
       gif: "tictactoeRobot.webp",
       tech: ["OpenCV", "Python", "RaspBerry Pi", "Template Matching"],
+      github:"https://github.com/Hemant-Mooneea/PDE3821-Tic-Tac-Toe-Algo"
     },
     {
       id: 2,
@@ -32,56 +33,69 @@ const Projects = () => {
       description: "A custom YOLOv8 model that has been trained to accurately identify uno cards (individual or bundle) as part as our robotics coursework.",
       gif: "unoModel.png",
       tech: ["Machine Learning", "Python", "Multiclass Classification", "Google Colab", "OpenCV"],
+      github:"https://github.com/Hemant-Mooneea/PDE3821-UNO-Card-detector"
     },
     {
       id: 3,
+      title: "Portfolio website",
+      description: "A simple portfolio website made using Next.js and Tailwind CSS to showcase my projects and skills acquired during my course.",
+      gif: "portfolio.webp",
+      tech: ["TypeScript", "Next.js", "TailwindCSS"],
+      github:"https://github.com/KrishiNund/Portfolio-Website"
+    },
+    {
+      id: 4,
       title: "Tuition website",
       description: "An after school tuition website to sell tuition classes as part of the FullStack Development coursework with frontend hosted using GitHub pages and backend hosted on Render.com.",
       gif: "tuitionweb.webp",
       tech: ["JavaScript", "Vue.js", "Render", "Dynamic Search"],
+      github:"https://github.com/KrishiNund/CST3144-M00934333-CW1-Frontend"
     },
     {
-      id: 4,
+      id: 5,
       title: "Game Website",
       description: "A Space Shooters game website where the player can create an account, play the game and compare his highest score against other players.",
       gif: "gameweb.webp",
       tech: ["JavaScript", "Node.js", "Express.js"],
+      github:"https://github.com/KrishiNund/Game-Website"
     },
     {
-      id: 5,
+      id: 6,
       title: "Social Media Website",
       description: "A game community website centered around 'Honkai Star Rail' which allows the user to post text/images/videos, follow other people, like, dislike and comment on their posts.",
       gif: "socialweb.webp",
       tech: ["JavaScript", "Node.js", "Express.js", "Bootstrap"],
+      github:"https://github.com/KrishiNund/Social-Media-Website"
     },
     {
-      id: 6,
+      id: 7,
       title: "Complex Calculator",
       description: "A multi-purpose calculator consisting of a basic, scientific, currency exchange, metric exchange and graphing calculator.",
       gif: "calc.webp",
       tech: ["Python", "OOP"],
+      github:"https://github.com/KrishiNund/Complex-Calculator"
     },
     {
-      id: 7,
+      id: 8,
       title: "Hangman",
       description: "Simple Hangman game where user has to guess a word according to the hint given.",
       gif: "hangman.webp",
       tech: ["Java", "Java Swing"],
+      github:"https://github.com/KrishiNund/Hangman"
     },
     {
-      id: 8,
+      id: 9,
       title: "Weather App",
       description: "Weather checing app where user can input a city name and they will obtain the temperature and weather description at that location.",
       gif: "weatherapp.webp",
       tech: ["C#", "Visual Studio", "Windows Form"],
+      github:"https://github.com/KrishiNund/Weather-App"
     },
-
-    
   ];
 
   return (
     <section id="projects" className="px-6 min-h-screen">
-      <div className="bg-gray-200 h-16 w-1 rounded-full dark:bg-opacity-50 mr-auto ml-auto" style={{opacity: 1, transform: "none", marginTop:-40}}></div>
+      <div className="bg-transparent h-16 w-1 rounded-full dark:bg-opacity-50 mr-auto ml-auto" style={{opacity: 1, transform: "none", marginTop:-40}}></div>
       <div className="max-w-6xl mx-auto text-center">
         {/* Title */}
         <h2 className="text-4xl font-bold highlight text-gray-800 mb-8 mt-10">
@@ -103,11 +117,25 @@ const Projects = () => {
 
               {/* Project Details */}
               <div className="mt-6 max-w-3xl text-center mr-auto ml-auto">
-                {/* Title */}
-                <h3 className="text-2xl font-semibold text-gray-800">
-                  {project.title}
-                </h3>
+                <div className="flex flex-row gap-x-2 items-center justify-center">
+                    {/* Title */}
+                  <h3 className="text-2xl font-semibold text-gray-800">
+                    {project.title}
+                  </h3>
 
+                  {/* GitHub Button */}
+                  <div>
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-block bg-transparent text-black px-4 py-2 rounded-full shadow hover:bg-gray-400 transition duration-300"
+                    >
+                      <i className="fa-brands fa-github fa-lg"></i>
+                    </a>
+                  </div>
+                </div>
+                
                 {/* Description */}
                 <p className="mt-4 text-gray-700 leading-relaxed px-4">
                   {project.description}
